2f7ab16185d6028d6bd8b77a4e71ccb88aba9b7a
katzkb <katsuya7kubo@gmail.com>
2026-03-23 03:36:22 +0900

    feat: add gct formula v0.0.3

---
+++ Formula/gct.rb
@@ -0,0 +1,32 @@
+class Gct < Formula
+  desc "Terminal UI tool for overseeing Git/GitHub workflows"
+  homepage "https://github.com/katzkb/git-control-tower"
+  version "0.0.3"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/katzkb/git-control-tower/releases/download/v#{version}/gct-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "fd677aa498992ae17220edef5da436d7272d0cabd0bd871d88a92a89c9609ae0"
+    else
+      url "https://github.com/katzkb/git-control-tower/releases/download/v#{version}/gct-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "9216a6c80a898ca25571a2ff34514eeb4480301bbcccb01efe6b5229cdd14d68"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/katzkb/git-control-tower/releases/download/v#{version}/gct-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "ae0870b5be37956ba48875423a05d946091e97c9949a858ee3d4c926d501cb4a"
+  end
+
+  depends_on "git"
+  depends_on "gh"
+
+  def install
+    bin.install "gct"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gct --version 2>&1", 1)
+  end
+end
